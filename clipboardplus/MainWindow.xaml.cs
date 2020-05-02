ef0fc841fe7bfce0843691788c8283a913cf6d5c
小憩然哀 <1135795134@qq.com>
2020-05-02 20:04:24 +0800

    complete functional zone ui

--- clipboardplus/MainWindow.xaml.cs
+++ clipboardplus/MainWindow.xaml.cs
@@ -23,6 +23,90 @@ namespace clipboardplus
         public MainWindow()
         {
             InitializeComponent();
+            SizeChanged += MainWindow_SizeChanged;
+            StateChanged += MainWindow_StateChanged;
+        }
+
+        private void MainWindow_StateChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {           
+                double clipWidth = SystemParameters.MaximumWindowTrackWidth;
+                double clipHeight = SystemParameters.MaximumWindowTrackHeight;
+                double gridWidth = SystemParameters.PrimaryScreenWidth;
+                double gridHeight = SystemParameters.PrimaryScreenHeight;
+                //MessageBox.Show(gridWidth + "    " + gridHeight + "    " + clipWidth + "     " + clipHeight);
+                windowClip.Rect = new Rect(0, 0, clipWidth, clipHeight);
+                globalGrid.Width = gridWidth;
+                globalGrid.Height = gridHeight;
+            }
+        }
+
+        private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                windowClip.Rect = new Rect(0, 0, this.Width, this.Height);
+                globalGrid.Width = this.Width;
+                globalGrid.Height = this.Height;
+            }
+        }
+
+        private void windowMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void windowDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = this.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        }
+
+        private void windowMin(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void showEditZone(object sender, MouseButtonEventArgs e)
+        {
+            if (this.Width == 300)
+            {
+                this.Width = 800;
+                this.Height = 450;
+            }
+            else if (this.Width == 800)
+            {
+                this.Width = 300;
+                this.Height = 450;
+            }
+        }
+
+        private void showPersonalCenter(object sender, MouseButtonEventArgs e)
+        {
+            personalCenterTab.IsSelected = true;
+        }
+
+        private void showSearchTab(object sender, RoutedEventArgs e)
+        {
+            searchTab.IsSelected = true;
+        }
+
+        private void advancedSearchShow(object sender, RoutedEventArgs e)
+        {
+            Grid.SetRowSpan(tabGrid, 1);
+        }
+
+        private void advancedSearchHide(object sender, RoutedEventArgs e)
+        {
+            Grid.SetRowSpan(tabGrid, 2);
+        }
+
+        private void showSearchTabKey(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            MessageBox.Show("key");
         }
     }
 }
