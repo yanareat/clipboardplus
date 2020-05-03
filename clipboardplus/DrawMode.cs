3e7451af4c0fbce1daac01130f27403f015f0f77
小憩然哀 <1135795134@qq.com>
2020-05-03 13:41:32 +0800

    add a paint tool

---
+++ clipboardplus/DrawMode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace clipboardplus
+{
+    public enum DrawMode
+    {
+        None = 0,
+        Select,
+        Pen,
+        Text,
+        Line,
+        Arrow,
+        Rectangle,
+        Circle,
+        Ray,
+        Erase
+    }
+}
